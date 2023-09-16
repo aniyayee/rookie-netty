0cd35edaf69aefd1d495077a8e24893d1522fac2
yayee <code.alpaca@qq.com>
2023-09-16 14:51:32 +0800

    refactor: 加入群聊 通知群内其他人

--- src/main/java/com/rookie/server/handler/GroupJoinRequestMessageHandler.java
+++ src/main/java/com/rookie/server/handler/GroupJoinRequestMessageHandler.java
@@ -9,6 +9,7 @@ import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import java.util.List;
 
 /**
  * @author yayee
@@ -21,14 +22,22 @@ public class GroupJoinRequestMessageHandler extends SimpleChannelInboundHandler<
         String groupName = msg.getGroupName();
         String username = msg.getUsername();
         Group group = GroupSessionFactory.getGroupSession().joinMember(groupName, username);
-        Channel channel = SessionFactory.getSession().getChannel(username);
+        Channel fromChannel = SessionFactory.getSession().getChannel(username);
         // 成功加入群聊
         if (group != null) {
-            channel.writeAndFlush(new GroupJoinResponseMessage(true, "加入群聊" + groupName + "成功"));
+            fromChannel.writeAndFlush(new GroupJoinResponseMessage(true, "加入群聊[" + groupName + "]成功"));
+            // 通知群内其他人
+            List<Channel> channels = GroupSessionFactory.getGroupSession().getMembersChannel(groupName);
+            for (Channel ch : channels) {
+                if (ch != fromChannel) {
+                    ch.writeAndFlush(
+                        new GroupJoinResponseMessage(true, "\"" + username + "\"加入了群聊[" + groupName + "]"));
+                }
+            }
         }
         // 加入群聊失败 群聊不存在
         else {
-            channel.writeAndFlush(new GroupJoinResponseMessage(false, "加入群聊" + groupName + "失败"));
+            fromChannel.writeAndFlush(new GroupJoinResponseMessage(false, "群聊[" + groupName + "]不存在"));
         }
     }
 }
